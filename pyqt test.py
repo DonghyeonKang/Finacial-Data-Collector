86886f4a59ac8e749ff5f96a6204f67258d2f670
DonghyeonKang <donghyeon3372@gmail.com>
2021-08-03 15:29:27 +0900

    Change List2 to table

--- pyqt test.py
+++ pyqt test.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QListWidget
-from PyQt5.QtGui import QFont
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QListWidget, QTableWidget, QTableWidgetItem, QTableView, QAbstractItemView
 from main import FDC
 
 class MyApp(QWidget, FDC):
@@ -8,6 +8,8 @@ class MyApp(QWidget, FDC):
         super().__init__()
         self.initUI()
         self.companies = []
+        self.tableitemnum = 0
+        self.selected_companies = []
 
     def initUI(self):
         # 윈도우 설정
@@ -20,50 +22,56 @@ class MyApp(QWidget, FDC):
         btn1 = QPushButton('회사 목록 업데이트', self)
         btn1.move(100, 650)
         btn1.setStyleSheet("background-color: grey")
-        btn1.setFont(QFont('SansSerif', 15))
+        btn1.setFont(QtGui.QFont('SansSerif', 15))
         btn1.resize(400, 80)
-        btn1.clicked.connect(self.update_list1)
+        btn1.clicked.connect(self.update_list)
         # 버튼2 
         btn2 = QPushButton('데이터 불러오기', self)
         btn2.move(900, 650)
         btn2.setStyleSheet("background-color: grey")
-        btn2.setFont(QFont('SansSerif', 15))
+        btn2.setFont(QtGui.QFont('SansSerif', 15))
         btn2.resize(400, 80)
-        btn1.clicked.connect(self.update_list2)
-        # 리스트 박스1
-        self.list1 = QListWidget(self)
-        self.list1.resize(400, 500)
-        self.list1.move(100, 50)
-        self.list1.itemDoubleClicked.connect(self.add_to_list2)
-        # 리스트 박스2
-        self.list2 = QListWidget(self)
-        self.list2.resize(700, 500)
-        self.list2.move(600, 50)
-        self.list2.insertItem(0, "qweq")
-        self.list2.insertItem(1, "qweq")
-        self.list2.insertItem(2, "qweq")
-        self.list2.itemDoubleClicked.connect(self.del_from_list2)
-        
+        btn1.clicked.connect(self.update_table)
+        # 리스트
+        self.list = QListWidget(self)
+        self.list.resize(400, 500)
+        self.list.move(100, 50)
+        self.list.itemDoubleClicked.connect(self.add_to_table)
+        # 테이블
+        self.table = QTableWidget(self)
+        self.table.setRowCount(0)
+        self.table.setColumnCount(7)
+        self.table.showGrid()
+        self.table.resize(800, 500)
+        self.table.move(520, 50)
+        column_headers = ['회사명', '주가', '시가총액', 'PER', 'BPS', '단기차입금', '장기차입금']
+        self.table.itemDoubleClicked.connect(self.del_from_table)
+        self.table.setHorizontalHeaderLabels(column_headers)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        # 보여주기
         self.show()
 
-    def update_list1(self):
+    def update_list(self):
         fdc = FDC()
         self.companies = fdc.update_company_list()
         for i in self.companies:
-            self.list1.addItem(i)
+            self.list.addItem(i)
 
-    def update_list2(self):
+    def update_table(self):
         pass
 
-    def add_to_list2(self):
-        item = self.list1.currentIndex()
-        self.list2.addItem(self.companies[item.row()])
+    def add_to_table(self):
+        tmpitem = self.list.currentIndex()
+        self.tableitemnum += 1
+        self.table.setRowCount(self.tableitemnum)
+        item = QTableWidgetItem(self.companies[tmpitem.row()])
+        self.table.setItem(self.tableitemnum - 1, 0, item)
+        self.selected_companies.append(self.companies[tmpitem.row()])
 
-    def del_from_list2(self):
-#        item = self.list1.currentIndex()
-        listItems = self.list2.selectedItems()
-        for item in listItems:
-            self.list2.takeItem(self.list2.row(item))
+    def del_from_table(self):
+        self.table.removeRow(self.table.currentRow())
+        self.tableitemnum -= 1
 
 
 
